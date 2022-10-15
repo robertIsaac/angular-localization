5f3fff3e965da3d3687ffa746b698e28c178a1d6
Robert Isaac (c) <irobert@vmware.com>
2022-10-15 11:07:02 +0300

    if the routes is translated, this should be the code
    this has a downside, that the links are not anchor tag but a button

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalizeRouterService } from '@gilsdav/ngx-translate-router';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,26 +9,15 @@ import { filter } from 'rxjs/operators';
 export class HeaderComponent implements OnInit {
   isCollapsed = true;
   locales = this.localizeRouterService.parser.locales;
-  currentUrl = '';
 
   constructor(
     private localizeRouterService: LocalizeRouterService,
-    private router: Router,
   ) { }
 
   ngOnInit(): void {
-    this.setCurrentUrl();
-
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-    ).subscribe(() => {
-      this.setCurrentUrl();
-    });
   }
 
-  private setCurrentUrl(): void {
-    this.currentUrl = this.router.url
-      .replace('/' + this.localizeRouterService.parser.currentLang, '')
-      .split('?')[0];
+  changeLang(locale: string): void {
+    this.localizeRouterService.changeLanguage(locale);
   }
 }
