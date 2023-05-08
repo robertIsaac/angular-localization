1dabad90668ac2e2111da3ae669d0da8f5413073
robertIsaac <dev.robert.isaac@gmail.com>
2023-05-08 16:36:02 +0300

    chore: run prune-ng-modules generator

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { FeatureComponent } from './feature.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   imports: [
-    SharedModule,
     FeatureRoutingModule,
     FeatureComponent,
-  ],
+],
 })
 export class FeatureModule {
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   imports: [
-    SharedModule,
     HomeRoutingModule,
     HomeComponent,
-  ],
+],
 })
 export class HomeModule {
 }
